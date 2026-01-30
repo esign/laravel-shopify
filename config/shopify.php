5f27e5fc3ae6e4feecc24937e99339ea0d0140ed
Cedric Vleminckx <vleminckx.cedric@hotmail.com>
2026-01-30 16:02:20 +0100

    feat: update to latest stable shopify api version

--- config/shopify.php
+++ config/shopify.php
@@ -9,7 +9,7 @@ return [
 
     'api_key' => env('SHOPIFY_API_KEY'),
     'api_secret' => env('SHOPIFY_API_SECRET'),
-    'api_version' => env('SHOPIFY_API_VERSION', '2025-01'),
+    'api_version' => env('SHOPIFY_API_VERSION', '2026-01'),
 
     /*
     |--------------------------------------------------------------------------
