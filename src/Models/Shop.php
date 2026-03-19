b95c772db959334a88e6074a9a8dd311b1906301
Cedric Vanhaegenberg <cedric.vanhaegenberg@dynamate.be>
2026-03-19 12:25:37 +0100

    chore(fix): resolve factory namespace

--- src/Models/Shop.php
+++ src/Models/Shop.php
@@ -2,10 +2,13 @@
 
 namespace Esign\LaravelShopify\Models;
 
+use Esign\LaravelShopify\Database\Factories\ShopFactory;
+use Illuminate\Database\Eloquent\Attributes\UseFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+#[UseFactory(ShopFactory::class)]
 class Shop extends Authenticatable
 {
     use HasFactory;
